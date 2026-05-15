6491d0bd1a506053cd8b6c60615fcf43f5ecae82
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-05-16 04:37:47 +0800

    [GREEN] Implement Note Object Validation.

--- src/functions/validateNote.js
+++ src/functions/validateNote.js
@@ -1,7 +1,10 @@
-/**
- * @param {Object} formData 
- * @returns {boolean}
- */
 export default function validateNote(formData) {
+    if (!formData.title.trim()) {
+        return false;
+    }
+    if (formData.content.length > 1000) {
+        return false;
+    }
+    
     return true;
 }
